0970224563cf868ba1a59ad1b67f71f4cd99d13d
vinicius123131 <vini989073599@gmail.com>
2024-05-23 05:16:58 -0300

    feat: melhoria no output

--- arch_flow/output/OutputHandler.py
+++ arch_flow/output/OutputHandler.py
@@ -1,11 +1,29 @@
 from colorama import init, Fore, Style
 import time
 import random
+from rich.console import Console
+from rich.live import Live
+from rich.table import Table
+import os
+import shutil
 
-init(autoreset=True)  # config  only for windows
+init(autoreset=True)
 
+console = Console()
+class SingletonMeta(type):
+    _instances = {}
 
-class OutputHandler:
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+class OutputHandler(metaclass=SingletonMeta):
+    messages = []
+    def __init__(self):
+        self.message_model = True
+
     @staticmethod
     def format_message(type_message, message, color_type=Fore.RED, identification=2):
         character_limit = 100
@@ -27,25 +45,71 @@ class OutputHandler:
 
         top_line = f"{color_type}{'-' * character_limit} {Style.RESET_ALL}"
         bottom_line = f"{color_type}{'-' * character_limit} {Style.RESET_ALL}"
-        OutputHandler.random_pause()
         return f"{top_line}\n{formatted_message}\n{bottom_line}"
 
-    @staticmethod
-    def success_message(message):
-        print(OutputHandler.format_message("[📟success📟]", message, color_type=Fore.GREEN, identification=5))
+    def success_message(self, message):
+        if self.message_model:
+            status = "success"
+            self.messages = self.generate_output(message, status, self.messages)
+        else:
+            print(OutputHandler.format_message("[📟success📟]", message, color_type=Fore.GREEN, identification=5))
+
+    def information_message(self, message):
+        if self.message_model:
+            status = "infor"
+            self.messages = self.generate_output(message, status, self.messages)
+        else:
+            print(OutputHandler.format_message("[information]", message, color_type=Fore.LIGHTBLACK_EX, identification=3))
+
+    def alert_message(self, message):
+        if self.message_model:
+            status = "alert"
+            self.messages = self.generate_output(message, status, self.messages)
+        else:
+            print(OutputHandler.format_message("[alert message️]", message, color_type=Fore.YELLOW, identification=1))
 
     @staticmethod
-    def information_message(message):
-        print(OutputHandler.format_message("[information]", message, color_type=Fore.LIGHTBLACK_EX, identification=3))
+    def clear_screen():
+        os.system('cls' if os.name == 'nt' else 'clear')
 
     @staticmethod
-    def alert_message(message):
-        print(OutputHandler.format_message("[alert message️]", message, color_type=Fore.YELLOW, identification=1))
+    def message_green(message):
+        console.print(f"[bright_green]{message}[/bright_green]")
 
     @staticmethod
-    def random_pause(min_interval=0.1, max_interval=1.0):
-        pause_time = random.uniform(min_interval, max_interval)
-        time.sleep(pause_time)
+    def message_red(message):
+        console.print(f"[bold red]{message}[/bold red]")
+
+    @staticmethod
+    def message_yellow(message):
+        console.print(f"[bold yellow]{message}[/bold yellow]")
+
+    @staticmethod
+    def update_content(live, messages):
+        terminal_size = shutil.get_terminal_size()
+        table_width = terminal_size.columns
+        max_message_length = max(len(message['message']) for message in messages)
+
+        description_width = min(max_message_length, table_width - 30)
+
+        table = Table(show_header=True, header_style="bold magenta")
+        table.add_column("Steps", style="dim", width=description_width)
+        table.add_column("Status", width=10, justify="center")
+        table.add_column("Step", justify="center", width=10)
+
+        for step, message in enumerate(messages, start=1):
+            table.add_row(message['message'], message['status'], f"{step}/{len(messages)}")
+
+        live.update(table)
+
+    def generate_output(self, new_message, status, previous_messages):
+        messages = previous_messages[:]
+        new_message = {"message": new_message, "status": status}
+        messages.append(new_message)
+        with Live(console=console, refresh_per_second=4) as live:
+            self.clear_screen()
+            self.update_content(live, messages)
+        return messages
 
     def dictionary_of_standard_functions(self):
         return {'format_message': self.format_message,
